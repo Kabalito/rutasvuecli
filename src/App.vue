7714c3c82e47e4e44f9e0a1dd0a9596ec90db7f7
Luis Amaya <kabal.amaya@hotmail.com>
2022-01-09 22:30:18 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app>
+    
+    <v-toolbar app color="red" dark>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Rutas y Llamadas HTTP</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-tile to="/">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Principal</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile to="/usuarios">
+          <v-list-tile-action>
+            <v-icon>group</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Usuarios</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+      
+    </v-navigation-drawer>
+  </v-app>
+</template>
+
+<script>
+export default {  
+  data () {
+    return {
+      drawer:false
+    }
+  }
+}
+</script>
